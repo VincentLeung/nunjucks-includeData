cfba801712aae9c29e5bf9081ca97a70b361027b
Vincent Leung <vincent.leung@heathwallace.com>
2016-07-13 09:39:25 +0800

    add injectToRoot tag for json file injection

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+var RecursiveIterator = require('recursive-iterator');
+
 function IncludeDataExtension(env) {
     this.tags = ['includeData'];
+    this.keywords = {
+        as: 'as',
+        clean: 'clean',
+        file: 'file',
+        data: 'data',
+        namespace: 'namespace',
+        injectToRoot_as_: '__injectToRoot_as_',
+    };
 
     this.parse = function(parser, nodes, lexer) {
         var tok = parser.nextToken();
@@ -24,26 +34,26 @@ function IncludeDataExtension(env) {
                     tok.colno);
             }
             var template = parser.parseExpression();
-            var aliasName = (parser.skipSymbol('as')) ? parser.parsePrimary().value : '';
-            var clean = parser.skipSymbol('clean');
+            var aliasName = (parser.skipSymbol(this.keywords.as)) ? parser.parsePrimary().value : '';
+            var clean = parser.skipSymbol(this.keywords.clean);
             var dict = new nodes.Dict(template.lineno, template.colno);
             dict.addChild(new nodes.Pair(template.lineno,
                                           template.colno,
-                                          new nodes.Literal(template.lineno, template.colno, 'clean'),
+                                          new nodes.Literal(template.lineno, template.colno, this.keywords.clean),
                                           new nodes.Literal(template.lineno, template.colno, clean)));
             dict.addChild(new nodes.Pair(template.lineno,
                                           template.colno,
-                                          new nodes.Literal(template.lineno, template.colno, 'file'),
+                                          new nodes.Literal(template.lineno, template.colno, this.keywords.file),
                                           template));
             dict.addChild(new nodes.Pair(template.lineno,
                                           template.colno,
-                                          new nodes.Literal(template.lineno, template.colno, 'namespace'),
+                                          new nodes.Literal(template.lineno, template.colno, this.keywords.namespace),
                                           new nodes.Literal(template.lineno, template.colno, aliasName)));
             argArray.addChild(dict);
         }
         kwargs.addChild(new nodes.Pair(template.lineno,
                                       template.colno,
-                                      new nodes.Literal(template.lineno, template.colno, 'data'),
+                                      new nodes.Literal(template.lineno, template.colno, this.keywords.data),
                                       argArray));
         args.addChild(kwargs);
         return new nodes.CallExtension(this, 'run', args);
@@ -52,32 +62,56 @@ function IncludeDataExtension(env) {
     this.run = function(context, args) {
         for (var i = 0; i < args.data.length; i++) {
             var data = args.data[i];
-            try {
-                var fileObj = env.loaders[0].getSource(data.file);
-                if (fileObj) {
-                    var jsonData = JSON.parse(fileObj.src);
-                    if (data.namespace && data.clean) {
-                        context.ctx[data.namespace] = jsonData;
-                    } else {
-                        var ctx = context.ctx;
-                        if (data.namespace) {
-                            if (!context.ctx[data.namespace]) {
-                                context.ctx[data.namespace] = {};
-                            }
-                            ctx = context.ctx[data.namespace];
-                        }
-                        for (var key in jsonData) {
-                            ctx[key] = jsonData[key];
-                        }
-                    }
-                } else {
-                    console.log("error to load: " + data.file);
-                }
-            } catch (e) {
-                console.log(e);
-            }
+            var jsonData = this.readFile(data.file, context.ctx);
+            this.addData(context.ctx, jsonData, data.namespace, data.clean);
         }
     };
+
+    this.includeJson = function(root, ctx) {
+        for(let {parent, node, key, path, deep} of new RecursiveIterator(root)) {
+            if (key.startsWith(this.keywords.injectToRoot_as_)) {
+                var jsonData = this.readFile(node, ctx);
+                var namespace = key.substring(this.keywords.injectToRoot_as_.length);
+                this.addData(ctx, jsonData, namespace, true);
+            }
+        }
+        return root;
+    };
+
+    this.readFile = function(fileName, ctx) {
+        try {
+            var fileObj = env.loaders[0].getSource(fileName);
+            if (fileObj) {
+                var jsonData = JSON.parse(fileObj.src);
+                jsonData = this.includeJson(jsonData, ctx);
+                return jsonData;
+            } else {
+                console.log("error to load: " + data.file);
+            }
+        } catch (e) {
+            console.log(e);
+        }
+        return null;
+    };
+
+    this.addData = function(ctx, jsonData, namespace, clean) {
+        if (jsonData) {
+            if (namespace && clean) {
+                ctx[namespace] = jsonData;
+            } else {
+                if (namespace) {
+                    if (!ctx[namespace]) {
+                        ctx[namespace] = {};
+                    }
+                    ctx = ctx[namespace];
+                }
+                for (var key in jsonData) {
+                    ctx[key] = jsonData[key];
+                }
+            }            
+        }
+    };
+
 }
 
 module.exports = IncludeDataExtension;
